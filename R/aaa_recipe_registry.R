76218ee0c1101f8df92b83e049ac84c09c2039de
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-05-19 10:03:32 +1000

    Remove functions

--- R/aaa_recipe_registry.R
+++
@@ -1,9 +0,0 @@
-#' Return registered recipe metadata
-#'
-#' @return A named list describing registered recipes.
-#' @export
-.recipe_registry_env <- new.env(parent = emptyenv())
-
-recipe_registry <- function() {
-    as.list.environment(.recipe_registry_env, all.names = TRUE)
-}
