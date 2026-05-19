b4da2236d29eded0729a98f2be44e41b6159706c
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-05-19 13:46:03 +1000

    Add argumens

--- R/read_output.R
+++ R/read_output.R
@@ -4,10 +4,19 @@
 #' @param apsimx_file The path to the APSIMX output file (.apsimx)
 #' @param report The name of the report to read from the APSIMX output (
 #' default: "HarvestReport")
+#' @param years Optional numeric vector of years to filter the output by (e.g., 1996:2025)
+#' 
 #' @return A data frame containing the APSIMX output data
 #' @export
-read_output <- function(apsimx_file, report = "HarvestReport") {
+read_output <- function(
+    apsimx_file, 
+    report = "HarvestReport",
+    years = NULL) {
     stopifnot(is.character(apsimx_file), length(apsimx_file) == 1, file.exists(apsimx_file))
+    stopifnot(is.character(report), length(report) == 1)
+    if (!is.null(years)) {
+        stopifnot(is.numeric(years), length(years) >= 1)
+    }
     df <-  rapsimng::read_report(apsimx_file, report)
     if (!tibble::has_name(df, "Clock.Today")) {
         stop("Expected 'Clock.Today' column not found in APSIM output. Please check the model configuration.")
@@ -21,13 +30,16 @@ read_output <- function(apsimx_file, report = "HarvestReport") {
     if (!tibble::has_name(df, "Fertilisation")) {
         stop("Expected 'Fertilisation' column not found in APSIM output. Please check the model configuration.")
     }
-    df |>
+    df <- df |>
         dplyr::mutate(
             Clock.Today = as.Date(.data$Clock.Today),
             SowingDOY = as.numeric(as.Date(paste(.data$SowingDate, "-2011", sep = ""), format = "%d-%b-%Y")) - 
         as.numeric(as.Date("2010-12-31")),
             Year = as.numeric(format(.data$Clock.Today, "%Y")),
         )
-    
+    if (!is.null(years)) {
+        df <- df |> dplyr::filter(.data$Year %in% years)
+    }
+    df
 }
 
